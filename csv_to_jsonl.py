0601dd32c3146578c4a59876516d44621e1d4bc2
jvithub <jacob.vanderneut.99@gmail.com>
2024-07-04 15:15:50 +1000

    Added function to convert csv file to jsonl

---
+++ csv_to_jsonl.py
@@ -0,0 +1,19 @@
+import csv
+import json
+
+def csv_to_jsonl(csv_file_path, jsonl_file_path):
+    with open(csv_file_path, mode='r', encoding='utf-8') as csv_file, open(jsonl_file_path, mode='w', encoding='utf-8') as jsonl_file:
+        csv_reader = csv.reader(csv_file)
+        for row in csv_reader:
+            record = {
+                "date_of_transaction": row[0],
+                "amount": float(row[1]),
+                "description": row[2],
+                "account_balance": float(row[3])
+            }
+            jsonl_file.write(json.dumps(record) + '\n')
+
+# Example usage
+csv_file_path = 'CSVData_jacob.csv'
+jsonl_file_path = 'transactions.jsonl'
+csv_to_jsonl(csv_file_path, jsonl_file_path)
